5f5b224f62436a587e25c9f284dbc6ec077613da
Stephane Guinot <33290562+StephaneGNT@users.noreply.github.com>
2019-01-28 18:05:05 +0100

    Update gmailCredentials.js

--- back/gmailCredentials.js
+++ back/gmailCredentials.js
@@ -1,6 +1,6 @@
 const gmailCredentials = {
   user: "sguinot86@gmail.com",
-  pass: "New5Pass47Gmail-",
+  pass: "",
   adress: 'sguinot86@gmail.com',
   client_id:"1036600950464-bfnoalqtivtc589pu4allbu42p0hp4la.apps.googleusercontent.com",project_id:"hackaton-1540911845838",
   auth_uri:"https://accounts.google.com/o/oauth2/auth",
