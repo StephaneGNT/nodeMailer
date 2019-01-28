4fd79da21197ed4d462cc6ab023ff774206287e6
Stephane Guinot <33290562+StephaneGNT@users.noreply.github.com>
2019-01-28 18:06:18 +0100

    Update mailGunCredentials.js

--- back/mailGunCredentials.js
+++ back/mailGunCredentials.js
@@ -1,6 +1,6 @@
 const mailGunCredentials = {
-  api_key: "064de3f9baeea8a356d19c3294596622-b0aac6d0-e750589b",
-  domain: "sandbox222916c7cad24a4ebe5a627bddf0c52b.mailgun.org",
+  api_key: "",
+  domain: "",
   adress: 'sguinot86@gmail.com',
 }
 module.exports = mailGunCredentials;
