446287289760d634bf44bd3cc38414f8b99fb812
Stephane Guinot <33290562+StephaneGNT@users.noreply.github.com>
2019-01-28 18:05:51 +0100

    Update gmailCredentials.js

--- back/gmailCredentials.js
+++ back/gmailCredentials.js
@@ -1,7 +1,7 @@
 const gmailCredentials = {
-  user: "sguinot86@gmail.com",
-  pass: "",
-  adress: 'sguinot86@gmail.com',
+  user: 'mail adress',
+  pass: 'mail adress password',
+  adress: 'mail adress',
   client_id:"1036600950464-bfnoalqtivtc589pu4allbu42p0hp4la.apps.googleusercontent.com",project_id:"hackaton-1540911845838",
   auth_uri:"https://accounts.google.com/o/oauth2/auth",
   token_uri:"https://oauth2.googleapis.com/token",auth_provider_x509_cert_url:"https://www.googleapis.com/oauth2/v1/certs",client_secret:"H3xZanhT5jiVAzqMRB4orP0Q"
